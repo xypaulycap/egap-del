6eaec6cdb13caf285e5a59bfa2d71a29f1e9654b
xypaulycap <imafidon001@gmail.com>
2024-11-20 02:08:48 -0800

    auth-favicon-metedata

--- src/app/manual-checkout/page.js
+++ src/app/manual-checkout/page.js
@@ -1,9 +1,12 @@
 import SectionHeaders from "@/components/layout/SectionHeaders";
 
-export default function ManualPayment(){
-    return(
-        <section>
-            <SectionHeaders mainHeader={'Manual Payment'} />
-        </section>
-    )
+export default function ManualPayment() {
+  return (
+    <section className="max-w-xs mx-auto">
+      {/* <SectionHeaders mainHeader={"Manual Payment"} /> */}
+      <div className="">
+        <h2 className="text-center text-4xl font-bold bg-gradient-to-r from-sky-400 via-teal-400 to-orange-500 text-transparent bg-clip-text italic">Please make payments to any of the accounts provided below</h2>
+      </div>
+    </section>
+  );
 }
